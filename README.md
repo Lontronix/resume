0b8c02204f6e1f1a1f78a8839c151168912f2a90
Lonnie Gerol <lonnie@bryx.com>
2022-11-29 12:46:42 -0500

    Adds build.sh to allow for different accent colors

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+# Lonnie's Personal Resume
 This is the repository for my personal resume, written in latex.
+
+## Compilation
+In the root directory of the project, run `./build.sh`. This will create 6 PDFs,
+each with a different accent color.
---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# https://tex.stackexchange.com/questions/270755/passing-arguments-to-latex-file
+
+rm -rf out/
+mkdir out 
+
+pairs=(
+	"green 5ebd3e"
+	"yellow ffb900"
+	"orange f78200"
+	"red e23838"
+	"purple 973999"
+	"blue 009cdf"
+)
+
+for pair in "${pairs[@]}"; do
+	color=$(echo $pair | awk '{ print $1 }')
+	hex=$(echo $pair | awk '{ print $2 }')
+	mkdir out/$color
+	pdflatex \
+		-jobname gerol-resume \
+		-output-directory out/$color/ \
+		"\\def\\accentHex{$hex}\\include{resume}"
+	rm -rf out/$color/gerol-resume.{aux,bcf,dvi,log,run.xml,out}
+done
